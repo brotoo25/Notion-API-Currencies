ca26edaa450c4e0e821f6366490693a76af6bc0b
Broto <brotoo25@gmail.com>
2022-01-24 00:06:11 -0300

    Update Notion dependency to latest

--- updateapp.sh
+++ updateapp.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
+
 docker stop notion-currency
 docker rm notion-currency
 
 docker rmi brotoo25/notion-currency-docker:latest
 docker pull brotoo25/notion-currency-docker:latest
 
-docker run -t -d --name notion-currency -e NOTION_TOKEN=$1 -e NOTION_DATABASE_ID=$2 -e NOTION_DEFAULT_CURRENCY=$3 brotoo25/notion-currency-docker:latest
+docker run -t -d --name notion-currency -e NOTION_TOKEN="$1" -e NOTION_DATABASE_ID="$2" -e NOTION_DEFAULT_CURRENCY="$3" brotoo25/notion-currency-docker:latest
